55198e671c028289ad8bd0d2bfb262760f0cd015
RANA ASHRAF <ra3748797@gmail.com>
2024-10-29 17:46:13 +0300

    update text input field

--- lib/shared_components/text_input_field.dart
+++ lib/shared_components/text_input_field.dart
@@ -12,36 +12,64 @@ class TextInputField extends StatelessWidget {
     this.label = '',
     this.labelColor = Colors.blue,
     this.suffixIcon,
+    this.enabled = true,
+    this.borderColor = Colors.blue,
+    this.disabledBorderColor = Colors.blue,
+    this.disabledborderRadius = 0,
+    this.enabledBorderColor = Colors.blue,
+    this.enabledborderRadius = 0,
+    this.focusedborderRadius = 0,
+    this.contentPadding,
+    this.suffixIconColor= Colors.blue,
   });
 
   final TextEditingController? controller;
   final String hintText;
   final Color hintColor;
-  final Color focusedBorderColor;
+  final bool enabled;
+  final Color borderColor;
   final double borderRadius;
+  final Color disabledBorderColor;
+  final double disabledborderRadius;
+  final Color enabledBorderColor;
+  final double enabledborderRadius;
+  final Color focusedBorderColor;
+  final double focusedborderRadius;
   final ValueChanged<String> onSubmitted;
   final String label;
   final Color labelColor;
   final IconData? suffixIcon;
+  final Color? suffixIconColor;
+  final EdgeInsetsGeometry? contentPadding;
+
   @override
   Widget build(BuildContext context) {
     return TextField(
+      enabled: enabled,
       controller: controller,
       onSubmitted: onSubmitted,
       decoration: InputDecoration(
-          suffixIcon: Icon(suffixIcon),
-          label: Text(label),
-          labelStyle: TextStyle(color: labelColor),
-          hintText: hintText,
-          hintStyle: TextStyle(color: hintColor),
-          contentPadding:
-              const EdgeInsets.symmetric(vertical: 30, horizontal: 16),
-          border: OutlineInputBorder(
+        suffixIcon: Icon(suffixIcon),
+        suffixIconColor: suffixIconColor,
+        label: Text(label),
+        labelStyle: TextStyle(color: labelColor),
+        hintText: hintText,
+        hintStyle: TextStyle(color: hintColor),
+        contentPadding: contentPadding,
+        border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(borderRadius),
-          ),
-          focusedBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(borderRadius),
-              borderSide: BorderSide(color: focusedBorderColor))),
+            borderSide: BorderSide(color: borderColor)),
+        // enabledBorder: OutlineInputBorder(
+        //     borderRadius: BorderRadius.circular(enabledborderRadius),
+        //     borderSide: BorderSide(color: enabledBorderColor)),
+        // focusedBorder: OutlineInputBorder(
+        //   borderRadius: BorderRadius.circular(focusedborderRadius),
+        //   borderSide: BorderSide(color: focusedBorderColor),
+        // ),
+        // disabledBorder: OutlineInputBorder(
+        //     borderRadius: BorderRadius.circular(disabledborderRadius),
+        //     borderSide: BorderSide(color: disabledBorderColor)),
+      ),
     );
   }
 }
