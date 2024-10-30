3789a74eb101bb504c8a4f5831ceb43bb3e8877a
RANA ASHRAF <ra3748797@gmail.com>
2024-10-30 05:04:54 +0300

    add day1 for current day && rename to response && handle the response with all its cases

--- lib/services/weather_api_service.dart
+++ lib/services/weather_api_service.dart
@@ -3,15 +3,23 @@ import 'package:w/models/weather_model.dart';
 
 class WeatherApiService {
   final Dio dio;
-  final String requestPath = 'https://api.weatherapi.com/v1';
+  final String baseUrl = 'https://api.weatherapi.com/v1';
   final String key = '666ad2f6cc3d4d6baf4141302242710';
   WeatherApiService(this.dio);
 
   Future<WeatherModel> getWeather({required String city}) async {
-    Response respone =
-        await dio.get('$requestPath/forecast.json?key=$key&q=$city');
+    try {
+      Response respone =
+          await dio.get('$baseUrl/forecast.json?key=$key&q=$city&days=1');
 
-    WeatherModel weatherModel = WeatherModel.fromJson(respone.data);
-    return weatherModel;
+      WeatherModel weatherModel = WeatherModel.fromJson(respone.data);
+      return weatherModel;
+    } on DioException catch (e) {
+      final String errorMessage = e.response?.data['error']['message'] ??
+          'oops there is an error, try later!';
+      throw Exception(errorMessage);
+    } catch (e) {
+      throw Exception('oops there is an error, try later!');
+    }
   }
 }
