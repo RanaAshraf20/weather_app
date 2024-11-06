5da41de3e847197e98e5d3eb81d1597de50b8b3c
RANA ASHRAF <ra3748797@gmail.com>
2024-11-06 20:14:24 +0200

    make the bloc builder to listen to the cubit and build the widget depending on the comming state

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:w/components/app_bar.dart';
+import 'package:w/cubits/get_weather_cubit/get_weather_cubit.dart';
+import 'package:w/cubits/get_weather_cubit/get_weather_states.dart';
 import 'package:w/views/search_view.dart';
 import 'package:w/widgets/no_weather_body.dart';
 import 'package:w/widgets/weather_info_body.dart';
@@ -10,18 +13,26 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBarComponent(
-        title: 'Weather',
-        icon: Icons.search,
-        onPressed: () {
-          Navigator.of(context).push(
-            MaterialPageRoute(builder: (context) {
-              return const SearchView();
-            }),
-          );
-        },
-      ),
-      body: weatherModel==null ? const NoWeatherBody():const WeatherInfoBody(),
-    );
+        appBar: AppBarComponent(
+          title: 'Weather',
+          icon: Icons.search,
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(builder: (context) {
+                return const SearchView();
+              }),
+            );
+          },
+        ),
+        body: BlocBuilder<GetWeatherCubit, WeatherStates>(
+            builder: (context, state) {
+          if (state is WeatherLoadedState) {
+            return const WeatherInfoBody();
+          } else if (state is WeatherFailerState) {
+            return Text(state.failerMessage);
+          } else {
+            return const NoWeatherBody();
+          }
+        }));
   }
 }
