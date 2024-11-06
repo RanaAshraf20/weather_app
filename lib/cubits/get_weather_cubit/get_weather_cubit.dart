001fbffee7c7c705462f328d04d861e995619458
RANA ASHRAF <ra3748797@gmail.com>
2024-11-06 20:04:06 +0200

    create get weather cubit

---
+++ lib/cubits/get_weather_cubit/get_weather_cubit.dart
@@ -0,0 +1,19 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:w/cubits/get_weather_cubit/get_weather_states.dart';
+import 'package:w/models/weather_model.dart';
+import 'package:w/services/weather_api_service.dart';
+
+class GetWeatherCubit extends Cubit<WeatherStates> {
+  GetWeatherCubit():super(InitialWeatherState());
+
+  getWeather({required String cityName}) async {
+    try {
+      WeatherModel weatherModel =
+          await WeatherApiService(Dio()).getCurrentWeather(city: cityName);
+      emit(WeatherLoadedState());
+    } catch (e) {
+      emit(WeatherFailerState( failerMessage: e.toString()));
+    }
+  }
+}
