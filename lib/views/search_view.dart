2639a078fa23503652dd43b02df08f944ae93b00
RANA ASHRAF <ra3748797@gmail.com>
2024-11-07 04:54:57 +0200

    use the normal app b ar so it take the defualt theme and it changes when the theme change

--- lib/views/search_view.dart
+++ lib/views/search_view.dart
@@ -11,32 +11,26 @@ class SearchView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const AppBarComponent(
-        title: 'Search',
+      appBar: AppBar(
+        title: const Text('search'),
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20.0),
         child: Center(
             child: TextInputField(
           onSubmitted: (value) async {
-            WeatherModel weatherModel =
-                BlocProvider.of<GetWeatherCubit>(context)
-                    .getWeather(cityName: value);
+            BlocProvider.of<GetWeatherCubit>(context)
+                .getWeather(cityName: value);
             Navigator.of(context).pop();
           },
           contentPadding:
               const EdgeInsets.symmetric(horizontal: 16, vertical: 32),
           label: 'Search',
-          labelColor: Colors.orange,
           suffixIcon: Icons.search,
           hintText: 'Enter City Name',
-          borderColor: Colors.orange,
-          suffixIconColor: Colors.orange,
           borderRadius: 6,
         )),
       ),
     );
   }
 }
-
-
